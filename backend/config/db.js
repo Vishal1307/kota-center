adac0a264b02f83e250dcd0e81f2f2eb198eceb5
vishal kumar <vishal.kuamr459@gmail.com>
2022-09-03 11:48:52 +0530

    init

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,9 @@
 const mongoose=require("mongoose")
 module.exports=()=>{
-    return mongoose.connect("mongodb://127.0.0.1:27017/kota")
+    return mongoose.connect("mongodb+srv://vishal1307:1307@cluster0.ddkdrj8.mongodb.net/kota?retryWrites=true&w=majority",{
+        // useCreateIndexes: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false,
+    }).then((res)=>console.log("Connection established")).catch((err)=>console.log("Connection error: " + err.message))
 }
